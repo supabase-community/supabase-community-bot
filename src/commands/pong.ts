9e258de4ffbf65d79b5b2bb6b9f6bc78e18a1db7
reddiedev <105657266+reddiedev@users.noreply.github.com>
2022-12-18 17:13:30 +0800

    changed payload to verbose form

--- src/commands/pong.ts
+++ src/commands/pong.ts
@@ -5,5 +5,5 @@ export const data = new SlashCommandBuilder()
   .setDescription('Replies with Pong!');
 
 export async function execute(interaction: CommandInteraction) {
-  await interaction.reply('Pong!');
+  await interaction.reply({ content: 'Pong!' });
 }
