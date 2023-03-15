dec8dce797a6448ad91d4221e83fedb6a6d14f8f
NanoCode012 <kevinvong@rocketmail.com>
2023-03-15 15:34:59 +0900

    Refactor to config object

--- src/commands/redirect.ts
+++ src/commands/redirect.ts
@@ -1,9 +1,6 @@
 import { CommandInteraction, SlashCommandBuilder } from 'discord.js';
 
-const questionsChannelId = '1006358244786196510';
-const redirectReplyMessage =
-  `Hello {user}! Please use the {channel} channel for questions. ` +
-  'You will find it easier to get answers there!';
+import { config } from '../config';
 
 export const data = new SlashCommandBuilder()
   .setName('redirect')
@@ -11,20 +8,16 @@ export const data = new SlashCommandBuilder()
     'Use this command to remind a user to use the proper question channel.'
   )
   .addUserOption(option =>
-    option
-      .setName('user')
-      .setDescription('The user you want to mention')
+    option.setName('user').setDescription('The user you want to mention')
   );
 
 export async function execute(interaction: CommandInteraction) {
   const userId = interaction.options.getUser('user')?.id;
-  
-  let msg = redirectReplyMessage.replace(
+
+  let msg = config.redirectReplyMessage.replace(
     '{user}',
     userId ? `<@${userId}>` : 'there'
   );
-
-  msg = msg.replace('{channel}', `<#${questionsChannelId}>`);
 
   await interaction.reply(msg);
 }
--- src/config.ts
+++ src/config.ts
@@ -9,3 +9,8 @@ export const env = envsafe({
   DISCORD_TOKEN: str(),
   CLIENT_ID: str(),
 });
+
+export const config = {
+  redirectReplyMessage:
+    'Hello {user}! Please use the <#1006358244786196510> channel for questions. You will find it easier to get answers there!',
+};
