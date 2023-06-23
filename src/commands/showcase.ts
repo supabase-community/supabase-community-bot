82a5312d8486e75cab0cb0c6afafe3cfb89e0344
Olyno <olyno.dev@gmail.com>
2023-06-23 19:35:32 +0200

    feat(showcase): add cooldown per user

--- src/commands/showcase.ts
+++ src/commands/showcase.ts
@@ -1,5 +1,23 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
+import {
+  ChatInputCommandInteraction,
+  PermissionsBitField,
+  SlashCommandBuilder,
+} from 'discord.js';
+import {
+  default_cooldowns,
+  getUserTimeLeft,
+  setUserCooldown,
+} from '../api/cooldowns';
 import { client } from '../supabase';
+
+function isValidUrl(url: string): boolean {
+  try {
+    new URL(url);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
 
 export const data = new SlashCommandBuilder()
   .setName('showcase')
@@ -59,12 +77,39 @@ export const data = new SlashCommandBuilder()
       )
   )
   .addSubcommand(subcommand =>
+    subcommand
+      .setName('cooldown')
+      .setDescription('Set a cooldown for command execution')
+      .addIntegerOption(option =>
+        option
+          .setName('duration')
+          .setDescription(
+            'The duration in seconds before you can execute the "showcase" command again'
+          )
+          .setRequired(false)
+      )
+  )
+  .addSubcommand(subcommand =>
     subcommand.setName('list').setDescription('List all projects you added')
   );
 
+export const cooldown = 3600;
+
 export async function execute(interaction: ChatInputCommandInteraction) {
+  const default_cooldown = default_cooldowns.get('showcase');
+
   switch (interaction.options.getSubcommand()) {
     case 'add':
+      const timeLeft = getUserTimeLeft(interaction.user.id, 'showcase');
+
+      if (timeLeft > 0) {
+        await interaction.reply({
+          content: `You can reuse this command in ${timeLeft} seconds`,
+          ephemeral: true,
+        });
+        break;
+      }
+
       const showcase = {
         name: interaction.options.getString('name'),
         description: interaction.options.getString('description'),
@@ -74,16 +119,6 @@ export async function execute(interaction: ChatInputCommandInteraction) {
         thumbnail: interaction.options.getString('thumbnail'),
         id_author: interaction.user.id,
       };
-
-      try {
-        new URL(showcase.link as string);
-      } catch (error) {
-        await interaction.reply({
-          content: 'Please provide a valid URL',
-          ephemeral: true,
-        });
-        return;
-      }
 
       const missing_fields = Object.entries(showcase)
         .filter(([key, value]) => {
@@ -97,6 +132,22 @@ export async function execute(interaction: ChatInputCommandInteraction) {
       if (missing_fields.length) {
         await interaction.reply({
           content: `Please fill all the required fields. Missing: ${missing_fields}`,
+          ephemeral: true,
+        });
+        return;
+      }
+
+      if (!isValidUrl(showcase.link as string)) {
+        await interaction.reply({
+          content: 'Please provide a valid URL',
+          ephemeral: true,
+        });
+        return;
+      }
+
+      if (showcase.thumbnail && !isValidUrl(showcase.thumbnail)) {
+        await interaction.reply({
+          content: 'Please provide a valid thumbnail URL',
           ephemeral: true,
         });
         return;
@@ -135,12 +186,63 @@ export async function execute(interaction: ChatInputCommandInteraction) {
         return;
       }
 
-      const { error } = await client.from('showcases').insert({
-        ...showcase,
-        id_message: embed.id,
+      const { error: errorShowcaseInsert } = await client
+        .from('showcases')
+        .insert({
+          ...showcase,
+          id_message: embed.id,
+        });
+
+      if (errorShowcaseInsert) {
+        await interaction.reply({
+          content: 'Something went wrong, please try again later',
+          ephemeral: true,
+        });
+        return;
+      }
+
+      await setUserCooldown(interaction.user.id, 'showcase');
+
+      await interaction.reply({
+        content: 'Your project has been added!',
+        ephemeral: true,
       });
 
-      if (error) {
+      break;
+
+    case 'cooldown':
+      const duration = interaction.options.getInteger('duration');
+
+      // Equivalent to a moderator or admin role
+      const hasPermission = interaction.member?.permissions.has([
+        PermissionsBitField.Flags.KickMembers,
+        PermissionsBitField.Flags.BanMembers,
+      ]);
+
+      if (!hasPermission) {
+        await interaction.reply({
+          content: 'You do not have the permission to use this command',
+          ephemeral: true,
+        });
+        return;
+      }
+
+      if (!duration) {
+        await interaction.reply({
+          content: `The cooldown is currently set to ${default_cooldown} seconds`,
+          ephemeral: true,
+        });
+        break;
+      }
+
+      const { error: errorCooldownUpsert } = await client
+        .from('commands')
+        .update({
+          cooldown: duration,
+        })
+        .eq('name', 'showcase');
+
+      if (errorCooldownUpsert) {
         await interaction.reply({
           content: 'Something went wrong, please try again later',
           ephemeral: true,
@@ -149,7 +251,7 @@ export async function execute(interaction: ChatInputCommandInteraction) {
       }
 
       await interaction.reply({
-        content: 'Your project has been added!',
+        content: `The cooldown has been set to ${duration} seconds`,
         ephemeral: true,
       });
 
